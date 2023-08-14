c89c3ab8842ef24946d2e9fda0a670cfdfc60a30
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-08-14 13:47:34 +0530

    login

---
+++ src/Pages/Login.js
@@ -0,0 +1,13 @@
+import './Login.css';
+import Header from '../Common/Header';
+
+const Login = () =>{
+    return(
+        <div>
+            <Header/>
+            This is login page.
+        </div>
+    );
+}
+
+export default Login;
