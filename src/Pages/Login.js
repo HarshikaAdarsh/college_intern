4271c6d94690713c06e4be9cfb159f7eab8823cb
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-08-14 14:32:25 +0530

    login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,25 @@
 import './Login.css';
 import Header from '../Common/Header';
+import{useState} from 'react';
 
 const Login = () =>{
+    let[username,setUsername]=useState("");
+    let[username,setUsername]=useState("");
+    function showData(){
+        alert(username);
+        alert(password);
+    }
     return(
         <div>
             <Header/>
             <div className='login-base'>
                 <div classname='login-main'>
-                    
+                    <div className='login-heading'>Login</div>
+                    <input type='text' className='login' placeholder='Username'
+                    onChange={(e)=>setUsername(e.target.value)} value={username}/>
+                   <input type='Password' className='login-input' placeholder='Password'
+                     onChange={(e)=>setPassword(e.target.value)} value={password}/>
+                    <button classNAme='login-button' onclick={showData}>Login</button>
                 </div>
             </div>
         </div>
